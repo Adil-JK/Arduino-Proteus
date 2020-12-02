62a8b12e5d7ca9fc9c8284510d6ac79876f45f34
Muhammad Adil <74587111+Adil-JK@users.noreply.github.com>
2020-12-02 06:04:54 -0800

    Add files via upload

---
+++ sevenSegment.ino
@@ -0,0 +1,62 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2, OUTPUT);   //a
+pinMode(3, OUTPUT);   //b
+pinMode(4, OUTPUT);   //c
+pinMode(5, OUTPUT);   //d
+pinMode(6, OUTPUT);   //e
+pinMode(7, OUTPUT);   //f
+pinMode(8, OUTPUT);   //g
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(3, HIGH);
+digitalWrite(4, HIGH);
+digitalWrite(1, LOW);
+digitalWrite(2, LOW);
+digitalWrite(5, LOW);
+digitalWrite(6, LOW);
+digitalWrite(7, LOW);
+digitalWrite(8, LOW);
+delay(1000);
+digitalWrite(3, LOW);
+digitalWrite(4, LOW);
+delay(100);
+
+
+digitalWrite(2, HIGH);
+digitalWrite(3, HIGH);
+digitalWrite(8, HIGH);
+digitalWrite(6, HIGH);
+digitalWrite(5, HIGH);
+digitalWrite(4, LOW);
+digitalWrite(7, LOW);
+delay(1000);
+digitalWrite(2, LOW);
+digitalWrite(3, LOW);
+digitalWrite(8, LOW);
+digitalWrite(6, LOW);
+digitalWrite(5, LOW);
+
+
+digitalWrite(3, HIGH);
+digitalWrite(4, HIGH);
+digitalWrite(1, LOW);
+digitalWrite(2, HIGH);
+digitalWrite(5, HIGH);
+digitalWrite(6, LOW);
+digitalWrite(7, LOW);
+digitalWrite(8, HIGH);
+delay(1000);
+digitalWrite(3, LOW);
+digitalWrite(4, LOW);
+digitalWrite(2, LOW);
+digitalWrite(5, LOW);
+digitalWrite(8, LOW);
+delay(100);
+
+delay(100);
+
+}
---
+++ seven_segment_binary.ino
@@ -0,0 +1,29 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(1, OUTPUT);   //a
+pinMode(2, OUTPUT);   //b
+pinMode(3, OUTPUT);   //c
+pinMode(4, OUTPUT);   //d
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(1, LOW);
+digitalWrite(2, LOW);
+digitalWrite(3, LOW);
+digitalWrite(4, LOW);
+delay(1000);
+
+digitalWrite(1, LOW);
+digitalWrite(2, LOW);
+digitalWrite(3, LOW);
+digitalWrite(4, HIGH);
+delay(1000);
+
+digitalWrite(1, LOW);
+digitalWrite(2, LOW);
+digitalWrite(3, HIGH);
+digitalWrite(4, LOW);
+delay(1000);
+}
