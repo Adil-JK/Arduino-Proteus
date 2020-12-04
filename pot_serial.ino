ca0359c1154321ddf1b87608a0582002966eacd3
Muhammad Adil <74587111+Adil-JK@users.noreply.github.com>
2020-12-04 07:08:41 -0800

    Add files via upload

---
+++ pot_serial.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int pot = analogRead(A0);
+
+float voltage = pot*(5.0/1023.0);
+
+Serial.println(voltage);
+delay(100);
+
+}
