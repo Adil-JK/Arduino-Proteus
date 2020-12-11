f70ec745bdd1bc551b9b2d572b90dd50f483065e
Muhammad Adil <74587111+Adil-JK@users.noreply.github.com>
2020-12-11 06:22:22 -0800

    Add files via upload

---
+++ ard-ard2.ino
@@ -0,0 +1,21 @@
+const int led = 5;
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(led, OUTPUT);
+}
+
+void loop() {
+
+bool value = Serial.read();
+if (value==true) {
+  digitalWrite(led, HIGH);
+}
+else {
+  digitalWrite(led, LOW);
+}
+ delay(100);
+
+}
---
+++ ard_ard.ino
@@ -0,0 +1,16 @@
+const int button = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(button, INPUT_PULLUP);
+}
+
+void loop() {
+  bool state = digitalRead(button);
+  if (state==true){
+  Serial.write(1);
+  }
+ delay(100);
+
+}
