78589998f7cf2012cf923c8b9a4f06773197b06e
Erdum <erdumadnan@gmail.com>
2020-12-13 21:37:41 +0500

    5th update

--- receiver.ino
+++ receiver.ino
@@ -1,7 +1,7 @@
 // The first mistake is that you try to store serial port data in type "integer", second mistake is that you already declared value variable on line 2, but you again declared it on line 12, now if i talk about your logic this simple toggling task can be done by more simple code
 // Finally i also cleaned up your code there are lots of styling mistake but styling is more advance thing you don't need think about it atleast for now
 const int led = 5;
-bool state;
+bool ledState false;
 char value;
 void setup() {
     Serial.begin(9600);
@@ -9,14 +9,14 @@ void setup() {
 }
 
 void loop() {
-    if(Serial.available()){
+    if(Serial.available() > 0){
         value = Serial.read();
     }
-    if (value == '1'){
-        state = true;
+    if(value == '1'){
+       ledState = true; 
     }
     else{
-        state = false;
+        ledState = false;
     }
-    digitalWrite(led, state);
+    digitalWrite(led, ledState);
 }
--- transmitter.ino
+++ transmitter.ino
@@ -1,24 +1,30 @@
 // First mistake ia your conditions after line 18 cannot be true because it is inside the main if condition
 const int button = 3;
-bool btnState;
 bool state = true;
-
 void setup() {
     Serial.begin(9600);
     pinMode(button, INPUT);
 }
 
 void loop() {
-  btnState = digitalRead(button);
-  if(btnState == true){
-      if(state){
-          Serial.write('1');
-          state = false;
-      }
-      else{
-          Serial.write('2');
-          state = true;
-      }
+    if(debounce(button)){
+        if(state){
+            Serial.write('1');
+            state = false;
+        }
+        else{
+            Serial.write('2');
+            state = true;
+        }
+    }
+}
+// This is our debouncing function
+bool debounce(int buttonPin){
+  bool stateNow = digitalRead(buttonPin);
+  if(stateNow == HIGH)
+  {
+    delay(50);
+    stateNow = digitalRead(buttonPin);
   }
-  
+  return stateNow;
 }
